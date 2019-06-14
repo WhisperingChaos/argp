cc50568eae9a74dde4b3b25d5fcf8ccffd70e48d
Rich Moyse <rich@moyse.us>
2019-06-14 15:36:35 -0400

    fix assert of arg count so expected count is first parameter - also put in bad assert to ensure codeship badge reflects bad status.

--- argp_test.go
+++ argp_test.go
@@ -15,31 +15,31 @@ func TestNoArgument(t *testing.T) {
 	args, err = Parse(strings.NewReader(""))
 	assrt.Nil(err)
 	assrt.Nil(args)
-	assrt.Equal(len(args), 0)
+	assrt.Equal(0, len(args))
 	args, err = Parse(strings.NewReader(" "))
 	assrt.Nil(err)
 	assrt.Nil(args)
-	assrt.Equal(len(args), 0)
+	assrt.Equal(0, len(args))
 }
 func TestArgEncapSingle(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`"abc"`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`abc`, args[0])
 }
 func TestArgEncapSingleWithEmbeddedDblQuoteAndEscape(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`"ab\\c\""`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`ab\c"`, args[0])
 }
 func TestArgEncapTwo(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`"abc" "def"`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 2)
+	assrt.Equal(2, len(args))
 	assrt.Equal(`abc`, args[0])
 	assrt.Equal(`def`, args[1])
 }
@@ -47,35 +47,35 @@ func TestArgEncapSingleQuoteNoReplaceEscapeSingle(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`'a\"b\\c'`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`a\"b\\c`, args[0])
 }
 func TestArgEncapSingleQuoteSingle(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`'abc'`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`abc`, args[0])
 }
 func TestArgNotEncap(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`abc`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`abc`, args[0])
 }
 func TestArgNotEncapWithEmbeddedDblQuoteEscapeSingleQuote(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`\"a\\b\'c\l`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`"a\b'cl`, args[0])
 }
 func TestArgNotEncapTwo(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`abc def`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 2)
+	assrt.Equal(2, len(args))
 	assrt.Equal(`abc`, args[0])
 	assrt.Equal(`def`, args[1])
 }
@@ -83,14 +83,14 @@ func TestArgEncapSingleDoubleOne(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`"abc"'def'`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`abcdef`, args[0])
 }
 func TestArgNotEncapStartWithWhitespaceTwo(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`abc def`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 2)
+	assrt.Equal(2, len(args))
 	assrt.Equal(`abc`, args[0])
 	assrt.Equal(`def`, args[1])
 }
@@ -98,14 +98,14 @@ func TestArgNotEncapAndEncapSingle(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`"abc"def`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`abcdef`, args[0])
 }
 func TestArgNotEncapAndEncapTrailingWhitespaceSingle(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`"abc"def     	`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`abcdef`, args[0])
 }
 func TestArgNotEncapSingleFail(t *testing.T) {
@@ -121,13 +121,20 @@ func TestArgNotEncapEscapeSingle(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`\\`))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
 	assrt.Equal(`\`, args[0])
 }
 func TestArgNotEncapTrailingWhitespace(t *testing.T) {
 	assrt := assert.New(t)
 	args, err := Parse(strings.NewReader(`a                                                                                                       `))
 	assrt.Nil(err)
-	assrt.Equal(len(args), 1)
+	assrt.Equal(1, len(args))
+	assrt.Equal(`a`, args[0])
+}
+func TestArgNotEncapLeadingWhitespaceSingle(t *testing.T) {
+	assrt := assert.New(t)
+	args, err := Parse(strings.NewReader(`        a                                                                                               `))
+	assrt.Nil(err)
+	assrt.Equal(0, len(args))
 	assrt.Equal(`a`, args[0])
 }
